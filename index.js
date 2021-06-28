49f6e7aaf48690d34dcaed80430819934825e409
Victor Bianchi <vbianchi90@gmail.com>
2021-06-28 11:07:17 -0400

    fix hardcoded port number

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const browser = require('browser-detect')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
